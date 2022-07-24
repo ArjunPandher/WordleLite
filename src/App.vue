0aeaba246ab9ba23be76dc83e031b41a6af23372
Arjun Pandher <42449099+ArjunPandher@users.noreply.github.com>
2022-07-24 15:33:52 -0400

    initial project setup, basic grid showing

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <WordleLite 
+    msg="Welcome to WordleLite"
+    :guessesAllowed=5
+    :wordLength=4
+  />
+</template>
+
+<script>
+import WordleLite from './components/WordleLite.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WordleLite
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+  place-items: center;
+  display: grid;
+}
+</style>
---
+++ src/components/WordleLite.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    {{ this.msg }}
+  </div>
+
+  <div id="board">
+    <template v-for="tileRow in guessesAllowed" :key="tileRow">
+      <div class="row">
+        <template v-for="tileTile in wordLength" :key="tileTile">
+          <div class="tile"></div>
+        </template>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordleLite',
+  props: {
+    msg: String,
+    guessesAllowed: Number,
+    wordLength: Number,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.row {
+  display: flex;
+}
+
+.tile {
+  width: 75px;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: 2px black;
+  outline-style: solid;
+  background-color: rebeccapurple;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
